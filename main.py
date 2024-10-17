f4aa2e0394a74d9c180e19606216df342d91c7f0
Gabriel Munakata <gabrielyuto2016@gmail.com>
2024-10-17 18:13:17 -0300

    main

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import rede
 import numpy as np
+import pandas as pd
+from sklearn import datasets
 
 if __name__ == '__main__':
-    input_neuron = 5
-    compete_neuron = 3
+  iris = datasets.load_iris()
+  input_neuron = 5
+  compete_neuron = 3
 
-    w, mask = rede.gennet_inh_lat(input_neuron, compete_neuron)
+  w, mask = rede.gennet_inh_lat(input_neuron, compete_neuron)
 
-    # Padrões: A, B, A, C, A
-    P = np.array([[0.1, 0.2, 0.0, 0.0, 0.7],
-                  [0.0, 0.0, 0.4, 0.6, 0.8],
-                  [0.3, 0.1, 0.0, 0.0, 0.6],
-                  [0.8, 0.2, 0.0, 0.0, 0.0],
-                  [0.2, 0.1, 0.0, 0.0, 0.7]])
+  df = pd.DataFrame(iris.data, columns=iris.feature_names)
+  padroes = np.array([df['sepal length (cm)'], df['sepal width (cm)'], df['petal length (cm)'], df['petal width (cm)']])
+  metade = padroes.shape[1] // 2
+  train_data = padroes[:, :metade]
+  # PADRÕES: | A | B | A | C | A |
+  # P = np.array([[0.1, 0.2, 0.0, 0.0, 0.7],
+  #               [0.0, 0.0, 0.4, 0.6, 0.8],
+  #               [0.3, 0.1, 0.0, 0.0, 0.6],
+  #               [0.8, 0.2, 0.0, 0.0, 0.0],
+  #               [0.2, 0.1, 0.0, 0.0, 0.7]]).T
 
-    train_data = np.array(P).T
-    matrix_weights, shift = rede.neural_network(w, mask, train_data)
+  # train_data = np.array(P)
+  matrix_weights, shift = rede.neural_network(w, mask, train_data)
 
-    # Teste A
-    input_test = np.array([0.1, 0.2, 0.0, 0.0, 0.7]).T
+  # TESTE: | A |
+  input_test = np.array([0.1, 0.2, 0.0, 0.0, 0.7]).T
 
-    result = rede.teste(matrix_weights, input_test, shift)
+  result = rede.teste(matrix_weights, input_test, shift)
 
-    print(result)
+  print(result)
--- rede.py
+++ rede.py
@@ -5,7 +5,9 @@ def gennet_inh_lat(input_neuron, compete_neuron):
   m12 = np.zeros((input_neuron, compete_neuron))
 
   m21 = 0.1 * np.ones((compete_neuron, input_neuron)) + 0.1 * np.random.rand(compete_neuron, input_neuron)
-  m22 = -(0.8 * (np.ones((compete_neuron, compete_neuron)) - np.eye(compete_neuron))) + 0.6 * np.eye(compete_neuron)
+  # m22 = -(0.8. * (ones(compet_neuron, compet_neuron) - eye(compet_neuron, compet_neuron))); -> Inhibicao lateral sem autapse
+  m22 = -(0.8 * (np.ones((compete_neuron, compete_neuron)) - np.eye(compete_neuron, compete_neuron))) # -> Inhibicao lateral sem autapse
+  # m22 = -(0.8 * (np.ones((compete_neuron, compete_neuron)) - np.eye(compete_neuron))) + 0.6 * np.eye(compete_neuron) #Acrescenta autapse
 
   w = np.block([[m11, m12], [m21, m22]])
 
@@ -18,51 +20,48 @@ def gennet_inh_lat(input_neuron, compete_neuron):
 def activation_function(activation, shift):
   return 1 / (1 + np.exp(-70 * (activation - shift)))
 
-
 def increment_w(output, output_antes, mask, w):
-    learning_factor = 0.001
-    term_a = np.dot(output, output_antes.T)
-    term_b = np.ones_like(output) * output_antes.T * w
+  learning_factor = 0.001
+  term_a = np.dot(output, output_antes.T)
+  term_b = np.ones_like(output) * output_antes.T * w
 
-    return (learning_factor * (term_a - term_b)) * mask
-
+  return (learning_factor * (term_a - term_b)) * mask
 
 def calculation_shift(output, shift):
-    displacement_speed = 0.025
+  displacement_speed = 0.025
 
-    return (displacement_speed * output + shift) / (1 + displacement_speed)
-
+  return (displacement_speed * output + shift) / (1 + displacement_speed)
 
 def neural_network(w, mask, train_data):
-    number_neurons = w.shape[0]
-    shift = 0.5 * np.ones((number_neurons, 1))
-    epochs = 1000
-    incw = np.zeros_like(w)
-    number_entries, patterns = train_data.shape
-    camadas = 1
+  number_neurons = w.shape[0]
+  shift = 0.5 * np.ones((number_neurons, 1))
+  epochs = 1000
+  incw = np.zeros_like(w)
+  number_entries, patterns = train_data.shape
+  camadas = 1
 
-    for i in range(epochs):
-        for j in range(patterns):
-            output = np.zeros((number_neurons, 1))
-            output_antes = output
+  for i in range(epochs):
+    for j in range(patterns):
+      output = np.zeros((number_neurons, 1))
+      output_antes = output
 
-            PAT = train_data[:, j]
-            output[0:number_entries, 0] = PAT
+      PAT = train_data[:, j]
+      output[0:number_entries, 0] = PAT
 
-            for k in range(camadas):
-                w = w + incw
-                inet = np.dot(w, output)
+      for k in range(camadas):
+        w = w + incw
+        inet = np.dot(w, output)
 
-                output = activation_function(inet, shift)
+        output = activation_function(inet, shift)
 
-                output = (inet > 0.0) * output
-                output[0:number_entries, 0] = PAT
+        output = (inet > 0.0) * output
+        output[0:number_entries, 0] = PAT
 
-                incw = increment_w(output, output_antes, mask, w)
-                shift = calculation_shift(output, shift)
-                output_antes = output
+        incw = increment_w(output, output_antes, mask, w)
+        shift = calculation_shift(output, shift)
+        output_antes = output
 
-    return w, shift
+  return w, shift
 
 def teste(w, input, shift):
   number_neurons = w.shape[0]
